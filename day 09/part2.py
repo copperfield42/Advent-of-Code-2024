8dd4e10e0f72488e7ec955d9ba6cdef7aed4e6b0
David E. Franco G. <david.elsensei3@gmail.com>
2024-12-10 12:17:59 -0400

    solution day 09 part2 v2

--- day 09/part2.py
+++ day 09/part2.py
@@ -5,9 +5,19 @@ from aoc_utils import test_input, get_raw_data, process_data, filesystem_checksu
 from tqdm_recipes import progress_bar
 
 
+def compact_free_space(disk: list[tuple[int | None, int]], index: int) -> None:
+    if disk[index][0] is not None:
+        return
+    if index + 1 < len(disk) and disk[index+1][0] is None:
+        acc = disk[index+1][1] + disk[index][1]
+        del disk[index+1]
+        disk[index] = (None, acc)
+    return compact_free_space(disk, index-1)
+
+
 def compact_disk_no_fragmentation(disk: list[tuple[int | None, int]]) -> None:
     for file_id in progress_bar((range(max(fid for fid, _ in disk if fid is not None)+1)[::-1])):
-        pos = next(i for i, (f, _) in enumerate(disk) if f == file_id)
+        pos = next(i for i, (f, _) in zip(reversed(range(len(disk))), reversed(disk)) if f == file_id)
         file = disk[pos]
         size = file[1]
         for node_pos in range(pos):
@@ -21,7 +31,8 @@ def compact_disk_no_fragmentation(disk: list[tuple[int | None, int]]) -> None:
         disk[node_pos] = file
         if free:
             disk.insert(node_pos+1, (None, free))
-        disk[:] = (next(value) if key is not None else (None, sum(v[1] for v in value)) for key, value in ir.groupby(disk, key=lambda x:x[0]))
+            pos += 1
+        compact_free_space(disk, pos)
 
 
 def main(data: str) -> int:
